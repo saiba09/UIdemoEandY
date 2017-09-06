149a6cd5ea8352cfd3cdb2c194ab54987a388aac
saiba09 <saiba_sheikh@persistent.co.in>
2017-09-06 11:49:08 +0530

    UI

--- src/main/webapp/scripts/config/env.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-    window.__env = window.__env || {};
-
-    // Base url
-	window.__env.apiUrl = "https://beta-006-dot-poc-iot.appspot.com/";
-	window.__env.avapiUrl = "https://beta-007-dot-poc-iot.appspot.com/";
-
-}(this));
